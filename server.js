e04bfa124175e380c483efab86e931895f46d81d
Vaibhav Jain <76522883+vaibhavjain111299@users.noreply.github.com>
2021-03-02 10:45:12 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/*", express.static(path.join(__dirname, "public")));
+//Server listing
+app.listen(process.env.port || 8081, (status) => {
+    console.log("server is running")
+});
